3f273a6036ef8c68b8690041b9c58b7e79c33476
MUKILTEO\1710220 <griffindbarlow@gmail.com>
2021-12-01 13:28:25 -0800

    Added some of the things

---
+++ Assets/SideMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SideMove : MonoBehaviour
+{
+    public float accel = 8;
+    private Rigidbody2D rb2;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move right
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            sr.flipX = false;
+            rb2.AddForce(new Vector2(accel, 0));
+        }
+
+        //Move right
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            sr.flipX = true;
+            rb2.AddForce(new Vector2(-accel, 0));
+        }
+
+    }
+}
